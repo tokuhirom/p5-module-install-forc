104a2134cf1ffe3d4c9c6ad76adb9a3d8a258a5f
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-10-01 23:26:56 +0900

    initial commit

---
+++ lib/Module/Install/ForC.pm
@@ -0,0 +1,33 @@
+package Module::Install::ForC;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Module::Install::ForC -
+
+=head1 SYNOPSIS
+
+  use Module::Install::ForC;
+
+=head1 DESCRIPTION
+
+Module::Install::ForC is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom  slkjfd gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
