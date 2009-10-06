b1eb55b29aee7e3037b944848dce44fb1d79ff35
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-10-06 22:47:58 +0900

    Checking in changes prior to tagging of version 0.05.  Changelog diff is:
    diff --git a/Changes b/Changes
    index ab2e793..648e356 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Revision history for Perl extension Module::Install::ForC
    
    +0.05
    +
    +    - win32 support(mattn++)
    +    - do not create a.out file in current dir!
    +
     0.04
    
         - $env->parse_config should return $env itself.

--- lib/Module/Install/ForC.pm
+++ lib/Module/Install/ForC.pm
@@ -1,7 +1,7 @@
 package Module::Install::ForC;
 use strict;
 use warnings;
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 use 5.008000;
 use Module::Install::ForC::Env;
 use Config;              # first released with perl 5.00307
